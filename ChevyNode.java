96fcaa21dad23843619feb594cb4481d2fe361cf
deancmattes <dean.mattes@sjsu.edu>
2022-12-02 13:37:08 -0800

    Added chevy trees and nodes

--- ChevyNode.java
+++ ChevyNode.java
@@ -1,12 +1,14 @@
 public class ChevyNode {
     int price;
     int year;
+    String model;
     ChevyCar car;
     ChevyNode left, right;
 
     public ChevyNode(ChevyCar c) {
         this.price = c.getPrice();
         this.year = c.getYear();
+        this.model = c.getModel();
         this.car = c;
         left = right = null;
     }

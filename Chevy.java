1efc020f92e3b34c465d2748d1727bdaddf920ec
deancmattes <dean.mattes@sjsu.edu>
2022-12-02 12:37:23 -0800

    Added chevy classes

---
+++ Chevy.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.Scanner;
+
+public class Chevy extends Vehicle{
+
+    @Override
+    public void randomCar() {
+
+    }
+
+    @Override
+    public void sortPrice(int min, int max) {
+
+    }
+
+    @Override
+    public void sortYear() {
+
+    }
+
+    @Override
+    public void soldCar(int price, int year, String model) {
+        ChevyCar temp = new ChevyCar(year, price, model);
+        addCar(temp);
+    }
+
+    private void addCar(ChevyCar temp) {
+        File chevyFile = new File("chevy.txt");
+        try {
+            FileWriter writer = new FileWriter("chevy.txt");
+            writer.write(temp.getYear() + "," + temp.getPrice() + "," + temp.getModel());
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Error in writing to chevy.txt");
+        }
+    }
+}

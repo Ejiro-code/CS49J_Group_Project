0415a991fa2b73b620df799910c4a99f326c426e
Niko Jokhadze <ngjokhadze@hotmail.com>
2022-12-02 12:51:02 -0800

    Switched the code between the two files to keep naming conventions consistent, plus more code for the Honda class.

--- Honda.java
+++ Honda.java
@@ -1,33 +1,112 @@
-public class Honda implements VehicleBuild{
-    private int year;
-    private int price;
-    private String make;
-    private String model;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.*;
 
-    public Honda(int year, int price, String make, String model) {
-        this.year = year;
-        this.price = price;
-        this.make = make;
-        this.model = model;
+public class Honda extends Vehicle {
+    public static ArrayList<HondaCar> cars = new ArrayList<>(15);
+
+    public static void something(String fileName) {
+        File honda = new File(fileName);
+
+        Scanner in;
+        {
+            try {
+                in = new Scanner(honda);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+            while (in.hasNextLine()) {
+                HondaCar car = new HondaCar();
+                String currentLine = in.nextLine();
+                Scanner readLine = new Scanner(currentLine);
+                readLine.useDelimiter(",");
+
+                if (readLine.hasNext()) {
+                    String current = readLine.next();
+                    int y = Integer.valueOf(current);
+                    car.setYear(y);
+                }
+                if (readLine.hasNext()) {
+                    String current = readLine.next();
+                    int y = Integer.valueOf(current);
+                    car.setPrice(y);
+                }
+
+                if (readLine.hasNext()) {
+                    String current = readLine.next();
+                    car.setModel(current);
+                }
+
+                cars.add(car);
+                readLine.close();
+            }
+        }
     }
 
-   // @Override
-    public String getMake() {
-        return this.make;
-    }
 
-    public int getYear() {
-        return year;
-    }
+    public void randomCar() {
+        File hondaList = new File("hondacars.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(hondaList);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
-    public int getPrice() {
-        return price;
+        // read each line and
+        // count number of lines
+        int count = 0;
+        while(sc.hasNextLine()) {
+            sc.nextLine();
+            count++;
+        }
+
+        sc.close();
+
+        System.out.println("Total Number of Lines: " + count);
+        Random rand = new Random();
+        int randNum = rand.nextInt(count);
+
+        System.out.print("The " + cars.get(randNum).getYear());
+        System.out.print(" Honda " + cars.get(randNum).getModel() + " was selected, ");
+        System.out.println("with a starting price of $" + cars.get(randNum).getPrice() + ".");
     }
 
     @Override
-    public String getModel() {
-        return this.model;
+    public void sortPrice(int min, int max) {
+
     }
 
+    @Override
+    public void sortYear() {
+        File hondaList = new File("hondacars.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(hondaList);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
+        String eachCar = new String();
+        LinkedList<String> carsByYear = new LinkedList<String>();
+        while (sc.hasNextLine()){
+            eachCar = sc.nextLine();
+            carsByYear.add(eachCar);
+        }
+        sc.close();
+
+
+        int count = 0;
+        Collections.reverse(carsByYear);
+        Iterator i = carsByYear.iterator();
+        while (i.hasNext()) {
+            System.out.println(i.next());
+        }
+    }
+
+    @Override
+    public void soldCar(int price, int year, String model) {
+
+    }
 }

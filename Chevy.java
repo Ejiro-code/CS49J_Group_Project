d3e73eda84e9a49b42c729c6c291ccf6316529da
deancmattes <dean.mattes@sjsu.edu>
2022-12-04 21:50:25 -0800

    Completed sorting by price and by year

--- Chevy.java
+++ Chevy.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.io.FileNotFoundException;
 import java.util.*;
 import java.util.Scanner;
@@ -16,13 +17,36 @@ public class Chevy extends Vehicle{
     }
 
     @Override
-    public void sortPrice(int min, int max) {
-
+    public String[] allCars() {
+        return new String[0];
     }
 
     @Override
-    public void sortYear() {
+    public String[] sortPrice(int min, int max) {
+        ArrayList<ChevyCar> aList = priceTree.printAscending();
+        ArrayList<ChevyCar> withinBoundsList = new ArrayList<ChevyCar>();
+        for (int i = 0; i < aList.size(); i++){
+            if (aList.get(i).getYear() >= min){
+                if (aList.get(i).getYear() <= max){
+                    withinBoundsList.add(aList.get(i));
+                } else break;
+            }
+        }
+        String[] arr = new String[withinBoundsList.size()];
+        for (int i = 0; i < withinBoundsList.size(); i++){
+            arr[i] = withinBoundsList.get(i).getYear() + " Chevy " + withinBoundsList.get(i).getModel() + " - $" + withinBoundsList.get(i).getPrice();
+        }
+        return arr;
+    }
 
+    @Override
+    public String[] sortYear() {
+        ArrayList<ChevyCar> aList = ageTree.printAscending();
+        String[] arr = new String[aList.size()];
+        for (int i = 0; i < aList.size(); i++){
+            arr[i] = aList.get(i).getYear() + " Chevy " + aList.get(i).getModel() + " - $" + aList.get(i).getPrice();
+        }
+        return arr;
     }
 
     @Override

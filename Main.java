36677b4588155e6b7de0ea1d7d8b62e7b158cc14
ejiroigun <ejiro.igun@sjsu.edu>
2022-12-05 17:45:34 -0800

    Final update for main

--- Main.java
+++ Main.java
@@ -1,13 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Honda s = new Honda();
-        Ford f = new Ford();
-        Chevy c = new Chevy("chevy.txt");
 
-        //When Niko and Ashton wrap up their methods, they can uncomment the next two lines respectively
-        //s.something("honda.txt");
-        //f.something("ford.txt");
-        //Dean doesn't have a method that reads the file as of yet
         mainWindow win = new mainWindow();
     }
 }

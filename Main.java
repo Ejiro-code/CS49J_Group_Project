9b06549158a488f0707e4bb8283e36e45d5c2650
deancmattes <dean.mattes@sjsu.edu>
2022-12-05 17:09:38 -0800

    in Main I changed Chevy c = new Chevy(); to Chevy c = new Chevy("chevy.txt");

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Honda s = new Honda();
         Ford f = new Ford();
-        Chevy c = new Chevy();
+        Chevy c = new Chevy("chevy.txt");
 
         //When Niko and Ashton wrap up their methods, they can uncomment the next two lines respectively
         //s.something("honda.txt");
